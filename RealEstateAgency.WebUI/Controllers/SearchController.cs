4e517167bd2cadf4abc04d785d02e1916cb11aaa
dackow <dackow@gmail.com>
2017-01-06 14:14:21 +0100

    Menu color changed. Search criteria part 1

---
+++ RealEstateAgency.WebUI/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using RealEstateAgency.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstateAgency.WebUI.Controllers
+{
+    
+    public class SearchController : Controller
+    {
+        private IOfferRepository repository;
+        public SearchController(IOfferRepository repo)
+        {
+            repository = repo;
+        }
+        public PartialViewResult SearchCriteria()
+        {
+            return PartialView();
+        }
+    }
+}
