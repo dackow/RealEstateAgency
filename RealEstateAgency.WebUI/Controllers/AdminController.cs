60164f68af4283ed68469385c8223cfa21eb52ce
dackow <dackow@gmail.com>
2017-01-26 23:01:08 +0100

    Adding images + editing (all on admin's site)

--- RealEstateAgency.WebUI/Controllers/AdminController.cs
+++ RealEstateAgency.WebUI/Controllers/AdminController.cs
@@ -55,11 +55,18 @@ namespace RealEstateAgency.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Offer offer)
+        public ActionResult Edit(Offer offer, IEnumerable<HttpPostedFileBase> files)
         {
             if (ModelState.IsValid)
             {
                 repository.SaveOffer(offer);
+
+                //if pictures exist then remove them first 
+                if (files != null && files.Count() > 0)
+                {
+                    DeleteImagesForOfferId(offer.Id);
+                    SavePhotosCreateThumbnails(files, offer.Id);
+                }
                 TempData["message"] = string.Format("Oferta '{0}' została zapisana", offer.Name);
                 return RedirectToAction("Index");
             }
@@ -88,62 +95,70 @@ namespace RealEstateAgency.WebUI.Controllers
             {
                 repository.SaveOffer(offer);
 
-                //process photo images
-                if (files != null)
+                ////process photo images
+                //if (files != null)
+                //{
+                //    string PATH_TO_SAVE_IMAGES = Server.MapPath("~/AgencyImages");
+                //    try
+                //    {
+                //        foreach (var photo in files)
+                //        {
+                //            string directory = Path.Combine(PATH_TO_SAVE_IMAGES, offer.Id.ToString());
+                //            if (!Directory.Exists(directory))
+                //            {
+                //                Directory.CreateDirectory(directory);
+                //            }
+
+                //            if (!Directory.Exists(directory))
+                //            {
+                //                throw new Exception("Can't find or create the directory : " + directory);
+                //            }
+                //            //string path = Path.Combine(PATH_TO_SAVE_IMAGES, Path.GetFileName(photo.FileName));
+
+                //            string fileName = Path.GetFileName(photo.FileName);
+                //            string fullPath = Path.Combine(directory, fileName);
+                //            photo.SaveAs(fullPath);
+
+                //            string thumb_directory = Path.Combine(PATH_TO_SAVE_IMAGES, offer.Id.ToString(), "thumb");
+                //            if (!Directory.Exists(thumb_directory))
+                //            {
+                //                Directory.CreateDirectory(thumb_directory);
+                //            }
+                //            if (!Directory.Exists(thumb_directory))
+                //            {
+                //                throw new Exception("Can't find or create the directory for thumbnails : " + directory);
+                //            }
+                //            else
+                //            {
+                //                string thumbFullPath = Path.Combine(thumb_directory, fileName);
+                //                //create a thumbnail for the image
+                //                using (Image im = Image.FromStream(photo.InputStream, true, true))
+                //                {
+                //                    int height = 200;// (im.Height / (im.Width / 200));
+                //                    using (Image thumb = im.GetThumbnailImage(200, height, () => false, IntPtr.Zero))
+                //                    {
+                //                        thumb.Save(thumbFullPath);
+                //                    }
+                //                }
+                //            }
+                //        }
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        TempData["message"] = "ERROR:" + ex.Message.ToString();
+                //        return RedirectToAction("Index");
+                //    }
+                //}
+
+                string msg = SavePhotosCreateThumbnails(files, offer.Id);
+                if (!string.IsNullOrEmpty(msg))
                 {
-                    string PATH_TO_SAVE_IMAGES = Server.MapPath("~/AgencyImages");
-                    try
-                    {
-                        foreach (var photo in files)
-                        {
-                            string directory = Path.Combine(PATH_TO_SAVE_IMAGES, offer.Id.ToString());
-                            if (!Directory.Exists(directory))
-                            {
-                                Directory.CreateDirectory(directory);
-                            }
-
-                            if (!Directory.Exists(directory))
-                            {
-                                throw new Exception("Can't find or create the directory : " + directory);
-                            }
-                            //string path = Path.Combine(PATH_TO_SAVE_IMAGES, Path.GetFileName(photo.FileName));
-
-                            string fileName = Path.GetFileName(photo.FileName);
-                            string fullPath = Path.Combine(directory, fileName);
-                            photo.SaveAs(fullPath);
-
-                            string thumb_directory = Path.Combine(PATH_TO_SAVE_IMAGES, offer.Id.ToString(), "thumb");
-                            if (!Directory.Exists(thumb_directory))
-                            {
-                                Directory.CreateDirectory(thumb_directory);
-                            }
-                            if (!Directory.Exists(thumb_directory))
-                            {
-                                throw new Exception("Can't find or create the directory for thumbnails : " + directory);
-                            }
-                            else
-                            {
-                                string thumbFullPath = Path.Combine(thumb_directory, fileName);
-                                //create a thumbnail for the image
-                                using (Image im = Image.FromStream(photo.InputStream, true, true))
-                                {
-                                    int height = (im.Height / (im.Width / 200));
-                                    using (Image thumb = im.GetThumbnailImage(200, height, () => false, IntPtr.Zero))
-                                    {
-                                        thumb.Save(thumbFullPath);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        TempData["message"] = "ERROR:" + ex.Message.ToString();
-                        return RedirectToAction("Index");
-                    }
+                    TempData["message"] = msg;
                 }
-
-                TempData["message"] = string.Format("{0} has been saved", offer.Name);
+                else
+                {
+                    TempData["message"] = string.Format("{0} has been saved", offer.Name);
+                }
                 return RedirectToAction("Index");
             }
             else
@@ -158,6 +173,64 @@ namespace RealEstateAgency.WebUI.Controllers
             }
         }
 
+        private string SavePhotosCreateThumbnails(IEnumerable<HttpPostedFileBase> files, int id)
+        {
+            string error = string.Empty;
+            if (files != null)
+            {
+                string PATH_TO_SAVE_IMAGES = Server.MapPath("~/AgencyImages");
+                try
+                {
+                    foreach (var photo in files)
+                    {
+                        string directory = Path.Combine(PATH_TO_SAVE_IMAGES, id.ToString());
+                        if (!Directory.Exists(directory))
+                        {
+                            Directory.CreateDirectory(directory);
+                        }
+
+                        if (!Directory.Exists(directory))
+                        {
+                            throw new Exception("Can't find or create the directory : " + directory);
+                        }
+                        //string path = Path.Combine(PATH_TO_SAVE_IMAGES, Path.GetFileName(photo.FileName));
+
+                        string fileName = Path.GetFileName(photo.FileName);
+                        string fullPath = Path.Combine(directory, fileName);
+                        photo.SaveAs(fullPath);
+
+                        string thumb_directory = Path.Combine(PATH_TO_SAVE_IMAGES, id.ToString(), "thumb");
+                        if (!Directory.Exists(thumb_directory))
+                        {
+                            Directory.CreateDirectory(thumb_directory);
+                        }
+                        if (!Directory.Exists(thumb_directory))
+                        {
+                            throw new Exception("Can't find or create the directory for thumbnails : " + directory);
+                        }
+                        else
+                        {
+                            string thumbFullPath = Path.Combine(thumb_directory, fileName);
+                            //create a thumbnail for the image
+                            using (Image im = Image.FromStream(photo.InputStream, true, true))
+                            {
+                                int height = 200;// (im.Height / (im.Width / 200));
+                                using (Image thumb = im.GetThumbnailImage(200, height, () => false, IntPtr.Zero))
+                                {
+                                    thumb.Save(thumbFullPath);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    error = "ERROR:" + ex.Message.ToString();
+                }
+            }
+            return error;
+        }
+
         public ActionResult Delete(int id)
         {
             Offer offer = repository.DeleteOffer(id);
@@ -165,6 +238,40 @@ namespace RealEstateAgency.WebUI.Controllers
             {
                 TempData["message"] = string.Format("Oferta '{0}' została usunięta", offer.Name);
             }
+            return RedirectToAction("Index");
+        }
+
+        private string DeleteImagesForOfferId(int id)
+        {
+            string info = string.Empty;
+            string PATH_TO_SAVE_IMAGES = Server.MapPath("~/AgencyImages");
+            string directory = Path.Combine(PATH_TO_SAVE_IMAGES, id.ToString());
+            if (!Directory.Exists(directory))
+            {
+                info = string.Format("Nie ma katalogu dla podanej oferty!");
+            }
+            else
+            {
+                Directory.Delete(directory, true);
+                info = string.Format("Zdjęcia zostały usunięte");
+            }
+            return info;
+        }
+        public ActionResult DeleteImages(int id)
+        {
+            //string PATH_TO_SAVE_IMAGES = Server.MapPath("~/AgencyImages");
+            //string directory = Path.Combine(PATH_TO_SAVE_IMAGES, id.ToString());
+            //if (!Directory.Exists(directory))
+            //{
+            //    TempData["message"] = string.Format("Nie ma katalogu dla podanej oferty!");
+            //}
+            //else
+            //{
+            //    Directory.Delete(directory, true);
+            //    TempData["message"] = string.Format("Zdjęcia zostały usunięte");
+            //}
+
+            TempData["message"] = DeleteImagesForOfferId(id);
             return RedirectToAction("Index");
         }
 
